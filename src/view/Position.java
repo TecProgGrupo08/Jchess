dfc83de03d3832504d2910439670ef7a8a870394
Ulysses <ulysses3353@gmail.com>
2016-09-10 18:45:54 -0300

    Applying style sheet in class Position.java

--- src/view/Position.java
+++ src/view/Position.java
@@ -8,7 +8,7 @@ public class Position {
 	private int squarePosition;
 
 	/**
-	 * calculates the graphical position out of the byte-square-board
+	 * calculates the graphical position out of the byte-square-board.
 	 * @param squarePos
 	 */
 	public Position(int squarePos){
@@ -16,9 +16,13 @@ public class Position {
 	  this.y=intMe(((squarePos-(squarePos%16))/16)+1);
 	  this.yString=intMe(this.y);
 	  this.xString=stringMe(this.x);
-    this.squarePosition=squarePos;
+	  this.squarePosition=squarePos;
 	}
 	
+	/**
+	 * calculates the graphical position out of the byte-square-board.
+	 * @param int - x, y
+	 */
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -27,6 +31,10 @@ public class Position {
 		this.squarePosition = ((this.x-1))+(intMe(this.y)-1)*16;
 	}
 
+	/**
+	 * calculates the graphical position out of the byte-square-board.
+	 * @param x, y
+	 */
 	public Position(String x, int y) {
 		this.xString = x;
 		this.yString = y;
@@ -34,7 +42,10 @@ public class Position {
 		this.y = intMe(y);
 		this.squarePosition = ((this.x-1))+(intMe(this.y)-1)*16;
 	}
-
+	/**
+	 * calculates the graphical position out of the byte-square-board.
+	 * @param String - string
+	 */
 	public Position(String string) {
 		char[] c = string.toCharArray();
 		if(c.length==2){
@@ -48,101 +59,179 @@ public class Position {
 	}
 
 
+	/**
+	 * Transform a letter position to a int value.
+	 * @param squarePos
+	 * @return int - a number 0 to 8
+	 */
+	
 	private int intMe(char valueOf) {
 		switch (valueOf){
-		case 'a': return 1;
-		case 'b': return 2;
-		case 'c': return 3;
-		case 'd': return 4;
-		case 'e': return 5;
-		case 'f': return 6;
-		case 'g': return 7;
-		case 'h': return 8;
-		default: return 0;
-				
-		}
-	}
-
-	private int intMe(int value) {
-		int zahl = value;
-		switch (zahl) {
-		case 1:
-			return 8;
-		case 2:
-			return 7;
-		case 3:
-			return 6;
-		case 4:
-			return 5;
-		case 5:
-			return 4;
-		case 6:
-			return 3;
-		case 7:
-			return 2;
-		case 8:
+		case 'a': {
 			return 1;
-		default:
+		}	
+		case 'b': {
+			return 2;
+		}
+		case 'c': {
+			return 3;
+		}	
+		case 'd': {
+			return 4;
+		}	
+		case 'e': {
+			return 5;
+		}	
+		case 'f': {
+			return 6;
+		}	
+		case 'g': { 
+			return 7;
+		}	
+		case 'h': {
+			return 8;
+		}	
+		default: {
 			return 0;
-
 		}
 
+		}
 	}
+	
+	/**
+	 * Transform a int position to a String position value.
+	 * @param xValue
+	 * @return int - a number 0 to 8
+	 */
 
 	private String stringMe(int xValue) {
 
 		int zahl = xValue;
 		switch (zahl) {
-		case 1:
+		case 1:{
 			return "a";
-		case 2:
+		}
+		case 2:{
 			return  "b";
-		case 3:
+		}
+		case 3:{
 			return  "c";
-		case 4:
+		}
+		case 4:{
 			return  "d";
-		case 5:
+		}
+		case 5:{
 			return  "e";
-		case 6:
+		}
+		case 6:{
 			return  "f";
-		case 7:
+		}
+		case 7:{
 			return  "g";
-		case 8:
+		}
+		case 8:{
 			return  "h";
-		default:
+		}
+		default:{
 			return null;
+		}
 
 		}
 
 	}
 
+	/**
+	 * Transform a int 0 to 8 to his opposite in a modular aritimetic of mod 9 
+	 * @param value
+	 * @return int - a number 0 to 8
+	 */
 	
+	private int intMe(int value) {
+		int zahl = value;
+		switch (zahl) {
+		case 1:{
+			return 8;
+		}
+		case 2:{
+			return 7;
+		}
+		case 3:{
+			return 6;
+		}
+		case 4:{
+			return 5;
+		}
+		case 5:{
+			return 4;
+		}
+		case 6:{
+			return 3;
+		}
+		case 7:{
+			return 2;
+		}
+		case 8:{
+			return 1;
+		}
+		default:{
+			return 0;
+		}
 
+		}
+
+	}
+	
+	/**
+	 * 	Get the x position 
+	 * @return int - x
+	 */
+	
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * 	Get the y position 
+	 * @return int - y
+	 */
+	
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * 	Get the String version of x position 
+	 * @return int - x
+	 */
 	public String getXName() {
 		return xString;
 	}
 
+	/**
+	 * 	Get the String version of y position 
+	 * @return int - y
+	 */
 	public int getYName() {
 		return yString;
 	}
 	
+	/**
+	 * 	Get the square position 
+	 * @return int - squarePosition
+	 */
 	public int getSquarePosition(){
 	  return squarePosition;
 	}
 
+	/**
+	 * 	Get the concatenated value go xString and yString 
+	 * @return int - x
+	 */
 	public String getName() {
 		if (xString != "null" && yString != 0) {
 			return xString + yString;
 		} else
-			return ("null");
+			return "null";
 	}
 
 }
