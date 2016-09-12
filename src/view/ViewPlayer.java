843d6c0448ec93d0b8f1e021b390363c10ba4cb4
Anderson Silva <andersoonn.ams@gmail.com>
2016-09-11 22:46:31 -0300

    Applying stylesheet to ViewPlayer.java

--- src/view/ViewPlayer.java
+++ src/view/ViewPlayer.java
@@ -1,10 +1,11 @@
 package view;
 
 public class ViewPlayer {
-  private boolean isHuman = true;
-  private int playerIntDifficulty = 0;
-  private int playerDepthLevel = 0;
-  private String playerName = "";
+  
+    private boolean isHuman = true;
+    private int playerIntDifficulty = 0;
+    private int playerDepthLevel = 0;
+    private String playerName = "";
   
   /**Creates a new ViewPlayer object give the playerName, the depth, and the int representing evaluator type
    * and a boolean isHuman, true if yes false if no
@@ -14,43 +15,47 @@ public class ViewPlayer {
    * @param playerIntDifficulty
    * @param isHuman
    */
-  public ViewPlayer (String playerName,int playerDepthLevel,int playerIntDifficulty,boolean isHuman){
-	  this.playerName = playerName;
-	  this.playerDepthLevel = playerDepthLevel;
-	  this.playerIntDifficulty = playerIntDifficulty;
-	  this.isHuman =isHuman;
-  }
+    public ViewPlayer ( String playerName,int playerDepthLevel,int playerIntDifficulty,boolean isHuman ){
+	  
+        this.playerName = playerName;
+    	this.playerDepthLevel = playerDepthLevel;
+    	this.playerIntDifficulty = playerIntDifficulty;
+    	this.isHuman =isHuman;
+    }
 
-  /**
-   * Method for determines if the player is human or not
-   * @return true if player is human, false otherwise
-   */
-  boolean isHuman() {
-	  return isHuman;
-  }
+    /**
+    * Method for determines if the player is human or not
+    * @return true if player is human, false otherwise
+    */
 
-  /**
-   * Gets the playerIntdifficulty, as specified in the constructor
-   * @return the value of the objects playerIntDifficulty variable
-   */
-  int getPlayerIntDifficulty() {
-	  return playerIntDifficulty;
-  }
+    boolean isHuman() {
+    	return isHuman;
+    }
 
-  /**
-   * Gets the depth level for this player
-   * @return playerDepthLevel as specified when the object was created
-   */
-  int getPlayerDepthLevel() {
-	  return playerDepthLevel;
-  }
+    /**
+    * Gets the playerIntdifficulty, as specified in the constructor
+    * @return the value of the objects playerIntDifficulty variable
+    */
 
-  /**
-   * Gets the player name
-   * @return
-   */
-  String getPlayerName() {
-	  return playerName;
-  }
+    int getPlayerIntDifficulty() {
+    	return playerIntDifficulty;
+    }
+
+    /**
+    * Gets the depth level for this player
+    * @return playerDepthLevel as specified when the object was created
+    */
+    
+    int getPlayerDepthLevel() {
+    	return playerDepthLevel;
+    }
+
+    /**
+    * Gets the player name
+    * @return
+    */
+    String getPlayerName() {
+    	return playerName;
+    }
 
 }
